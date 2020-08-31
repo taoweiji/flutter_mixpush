41e3fc3cb58f0220be89cf8e3f9542ab01e959f8
陶伟基 <taoweiji@thejoyrun.com>
2020-08-31 14:07:36 +0800

    Update SwiftFlutterMixPushPlugin.swift

--- ios/Classes/SwiftFlutterMixPushPlugin.swift
+++ ios/Classes/SwiftFlutterMixPushPlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public class SwiftFlutterMixPushPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_mixpush", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "com.mixpush/flutter_mixpush", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterMixPushPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
