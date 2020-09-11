723916f7c6a882d6e0542d190ca47364b594b1a5
Wiki <taoweiji2008@qq.com>
2020-09-11 14:25:49 +0800

    升级MixPush的SDK

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -27,7 +27,7 @@ class _MyAppState extends State<MyApp> {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      platformVersion = await FlutterMixpush.platformVersion;
+      platformVersion = await FlutterMixPush.platformVersion;
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
